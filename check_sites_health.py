8e20cf8b6d82160800a52e65800d0843d78f3013
Nicolay Poymanov <n.poymanov@gmail.com>
2018-09-05 20:42:31 +0300

    rename getters functions

--- check_sites_health.py
+++ check_sites_health.py
@@ -4,7 +4,7 @@ import sys
 import re
 import requests
 import whois
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def parse_args():
@@ -34,33 +34,39 @@ def validate_urls(urls_list):
     return True
 
 
-def get_http_status_description(url):
+def is_server_available(url):
     response = requests.get(url)
     return response.ok
 
 
-def get_expiration_date(url):
+def get_expiration_info(url):
     domain_info = whois.whois(url)
     return domain_info.expiration_date
 
 
 def get_expiration_description(expiration_date, expires_period=30):
-    if type(expiration_date) is not datetime:
+    if type(expiration_date) is list:
+        expiration_date = expiration_date[0]
+    elif type(expiration_date) is not datetime:
         return 'N/A'
 
-    date_delta = datetime.today() - expiration_date
+    today = datetime.today()
+    date_delta = today - expiration_date
     date_delta_days = date_delta.days
 
     if date_delta_days < 0 and abs(date_delta_days) >= expires_period:
-        description = 'Not expired'
+        today_format = today.strftime('%d-%m-%Y')
+        period = today + timedelta(days=expires_period)
+        period_format = period.strftime('%d-%m-%Y')
+        description = 'Not expired ({}-{})'.format(today_format, period_format)
     else:
         description = 'Expires'
 
     return description
 
 
-def get_http_status_description(http_status):
-    if http_status:
+def get_server_response_description(availability):
+    if availability:
         description = 'OK'
     else:
         description = 'Connection error'
@@ -74,12 +80,12 @@ def get_urls_info(urls_list):
     for url in urls_list:
         url_data = {}
 
-        http_status = get_http_status_description(url)
-        expiration_date = get_expiration_date(url)
+        availability = is_server_available(url)
+        expiration_info = get_expiration_info(url)
 
         url_data['url'] = url
-        url_data['http_status'] = get_http_status_description(http_status)
-        url_data['expiration'] = get_expiration_description(expiration_date)
+        url_data['http_status'] = get_server_response_description(availability)
+        url_data['expiration'] = get_expiration_description(expiration_info)
 
         urls_info.append(url_data)
 
